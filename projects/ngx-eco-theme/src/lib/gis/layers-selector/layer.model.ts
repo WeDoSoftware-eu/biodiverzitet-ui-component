ce267ddbae81ad8a08e7262c0b6cd73ceffbb5b5
Aleksadar Lang <aleksadnar.lang@gmail.com>
2026-01-13 23:24:25 +0100

    Revert "test(gis): add comprehensive unit tests for GIS components"
    This reverts commit d2955bdb5e0f46be4882f5204c5e201149b09a4c.

--- projects/ngx-eco-theme/src/lib/gis/layers-selector/layer.model.ts
+++ projects/ngx-eco-theme/src/lib/gis/layers-selector/layer.model.ts
@@ -10,5 +10,5 @@ export interface MapLayerGroup {
   id: string;
   name: string;
   layers: MapLayer[];
-  expanded: boolean; // false po defaultu - zatvoreno
+  expanded: boolean;  // false po defaultu - zatvoreno
 }
