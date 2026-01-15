39d8d5fe83ba0b0351782a2d483cc4b6fc37206f
Nenad Ralic <nenad.ralic@digitalware.rs>
2026-01-15 10:07:00 +0100

    Dropdown div

--- projects/ngx-eco-theme/src/lib/header/header.component.ts
+++ projects/ngx-eco-theme/src/lib/header/header.component.ts
@@ -4,7 +4,14 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { IconComponent } from '../icon/icon.component';
+import { EcoIcon, IconComponent } from '../icon/icon.component';
+
+interface profileItems {
+  id: number;
+  title: string;
+  link: string;
+  icon: EcoIcon;
+}
 
 @Component({
   selector: 'eco-header',
@@ -16,4 +23,19 @@ import { IconComponent } from '../icon/icon.component';
 export class HeaderComponent {
   title = input<string>('');
   subtitle = input<string>('');
+
+  profileLinks: profileItems[] = [
+    {
+      id: 1,
+      title: 'My Profile',
+      link: '/',
+      icon: 'cancel',
+    },
+    {
+      id: 2,
+      title: 'Log Out',
+      link: '/',
+      icon: 'cancel',
+    },
+  ];
 }
