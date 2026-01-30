d9cb2b06ea8025c740818c53d71b902560123a46
Aleksadar Lang <aleksadnar.lang@gmail.com>
2026-01-30 21:27:42 +0100

    feat(gis): add legend-selector component

--- projects/ngx-eco-theme/src/public-api.ts
+++ projects/ngx-eco-theme/src/public-api.ts
@@ -22,12 +22,14 @@ export * from './lib/gis/search/search.component';
 export * from './lib/gis/legend-button/legend-button.component';
 export * from './lib/gis/download-button/download-button.component';
 export * from './lib/gis/layers-selector/layers-selector.component';
+export * from './lib/gis/legend-selector/legend-selector.component';
 export * from './lib/gis/map-status-bar/map-status-bar.component';
 
 /* MODEL */
 export * from './lib/sidebar/nav-item/nav-item.model';
 export * from './lib/table/table.model';
 export * from './lib/gis/layers-selector/layer.model';
+export * from './lib/gis/legend-selector/legend.model';
 export * from './lib/gis/map-status-bar/map-status-bar.model';
 
 /* TOKEN */
