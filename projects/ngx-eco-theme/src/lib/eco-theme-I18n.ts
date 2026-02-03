22f0933c56802e2776f50fa8d6fa85d6bdb83361
marko-stojakovic <marko.stojakovic@birgtech.com>
2026-02-03 12:13:44 +0100

    translation for date picker

--- projects/ngx-eco-theme/src/lib/eco-theme-I18n.ts
+++ projects/ngx-eco-theme/src/lib/eco-theme-I18n.ts
@@ -36,6 +36,22 @@ export interface EcoThemeI18n {
     myProfile: string;
     logOut: string;
   };
+  datePicker: {
+    selectDate: string;
+    selectPeriod: string;
+    fromDate: string;
+    toDate: string;
+    switchToMultiYearViewLabel: string;
+    nextMonthLabel: string;
+    nextYearLabel: string;
+    nextMultiYearLabel: string;
+    prevMonthLabel: string;
+    prevYearLabel: string;
+    prevMultiYearLabel: string;
+    calendarLabel: string;
+    openCalendarLabel: string;
+    closeCalendarLabel: string;
+  };
 }
 
 export const ECO_THEME_I18N = new InjectionToken<EcoThemeI18n>('ECO_THEME_I18N');
@@ -75,5 +91,21 @@ export const DEFAULT_ECO_THEME_I18N: EcoThemeI18n = {
     profile: 'Профил',
     myProfile: 'Мој профил',
     logOut: 'Одјави се',
+  },
+  datePicker: {
+    selectDate: 'Изаберите датум',
+    selectPeriod: 'Изаберите период',
+    fromDate: 'Од датума',
+    toDate: 'До датума',
+    switchToMultiYearViewLabel: 'Изабери годину',
+    nextMonthLabel: 'Следећи месец',
+    nextYearLabel: 'Следећа година',
+    nextMultiYearLabel: 'Следећих 24 године',
+    prevMonthLabel: 'Претходни месец',
+    prevYearLabel: 'Претходна година',
+    prevMultiYearLabel: 'Претходних 24 године',
+    calendarLabel: 'Календар',
+    openCalendarLabel: 'Отвори календар',
+    closeCalendarLabel: 'Затвори календар',
   },
 };
