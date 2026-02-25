be50cb869ab1dda3441d7b8bc2c27ae5448eb377
Nenad Ralic <nenad.ralic@digitalware.rs>
2026-02-25 09:44:03 +0100

    feat: image-preview-dialog tests

--- projects/ngx-eco-theme/src/lib/image-preview-dialog/image-preview-dialog.component.spec.ts
+++ projects/ngx-eco-theme/src/lib/image-preview-dialog/image-preview-dialog.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ImagePreviewDialogComponent } from './image-preview-dialog.component';
 
@@ -8,7 +10,11 @@ describe('ImagePreviewDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ImagePreviewDialogComponent],
+      imports: [ImagePreviewDialogComponent, NoopAnimationsModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ImagePreviewDialogComponent);
