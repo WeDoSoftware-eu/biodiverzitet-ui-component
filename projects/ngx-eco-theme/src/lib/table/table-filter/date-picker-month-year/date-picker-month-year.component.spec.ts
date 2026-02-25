df91b1770d3f72bd4bfdc59395c906caf3b57d39
Nenad Ralic <nenad.ralic@digitalware.rs>
2026-02-25 09:32:17 +0100

    fix: date-picker-moht-year test

--- projects/ngx-eco-theme/src/lib/table/table-filter/date-picker-month-year/date-picker-month-year.component.spec.ts
+++ projects/ngx-eco-theme/src/lib/table/table-filter/date-picker-month-year/date-picker-month-year.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 import { DatePickerMonthYearComponent } from './date-picker-month-year.component';
 
@@ -9,6 +10,7 @@ describe('DatePickerMonthYearComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DatePickerMonthYearComponent],
+      providers: [provideNativeDateAdapter()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DatePickerMonthYearComponent);
