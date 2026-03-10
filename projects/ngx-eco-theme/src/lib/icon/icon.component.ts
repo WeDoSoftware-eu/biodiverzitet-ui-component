f945a83f0ae2051d354dc826697162818043a757
Nenad Ralic <nenad.ralic@digitalware.rs>
2026-03-10 09:17:07 +0100

    feat: new icons

--- projects/ngx-eco-theme/src/lib/icon/icon.component.ts
+++ projects/ngx-eco-theme/src/lib/icon/icon.component.ts
@@ -89,6 +89,16 @@ export const ECO_ICONS = [
   'warrning',
   'zoom-in',
   'zoom-out',
+  'chart',
+  'exclamation-mark-circle',
+  'chart',
+  'eye-invisible',
+  'folder-open',
+  'pdf',
+  'plus',
+  'plus-circle',
+  'warning-yellow',
+  'word',
 ] as const;
 
 export type EcoIcon = (typeof ECO_ICONS)[number];
